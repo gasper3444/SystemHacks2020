704679d2442c4d31287140215b94eb954632f131
Jordan Chong <jtchong@sfu.ca>
2020-02-08 10:23:47 -0800

    created event class

---
+++ app/src/main/java/com/example/systemhacks_2020/ObjectsForUI/Event.java
@@ -0,0 +1,12 @@
+package com.example.systemhacks_2020.ObjectsForUI;
+
+public class Event {
+    private String eventName;
+    private String creator;
+    private String date;
+    private double moneyOwed;
+
+    public Event() {
+
+    }
+}
